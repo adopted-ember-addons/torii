be91dd2b4262f975ab25517f47cc5e87d635860b
Jan Krutisch <jan@krutisch.de>
2022-06-03 20:41:16 +0200

    Remove logs

--- tests/unit/lib/uuid-generator-test.js
+++ tests/unit/lib/uuid-generator-test.js
@@ -1,10 +1,7 @@
 import { module, test } from 'qunit';
 import UUIDGenerator from '@adopted-ember-addons/torii/lib/uuid-generator';
 
-console.log('WAS HERE');
-
 module('Unit | Lib | UUIDGenerator', function () {
-  console.log('HERE AS WELL', test);
   test('exists', function (assert) {
     assert.ok(UUIDGenerator);
   });
